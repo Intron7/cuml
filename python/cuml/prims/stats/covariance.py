0686dac3baede81c5c822a6d61d2945cebd6d5e3
Intron7 <severin.dicks@icloud.com>
2023-07-12 15:00:54 +0200

    added full replacement for `_cov_sparse`

--- python/cuml/prims/stats/covariance.py
+++ python/cuml/prims/stats/covariance.py
@@ -36,8 +36,8 @@ cov_kernel_str = r"""
 }
 """
 
-mean_cov_kernel_str = r"""
-(const int *indptr, const int *index, {0} *data, int nrows, int ncols, {0} *out, {0} *mean) {
+cov_kernel_str_sparse_xx = r"""
+(const int *indptr, const int *index, {0} *data, int nrows, int ncols, {0} *out) {
     int row = blockDim.x * blockIdx.x + threadIdx.x;
     if(row >= nrows) return;
     int start_idx = indptr[row];
@@ -48,11 +48,36 @@ mean_cov_kernel_str = r"""
         {0} data1 = data[idx];
         long long int outidx = \
             static_cast<long long int>(index1) * ncols + index1;
-        atomicAdd(&out[outidx], data1 * data1);
-        atomicAdd(&mean[index1], data1);
+        atomicAdd(&out[outidx], data1 * data1 / 2);
         for(int idx2 = idx+1; idx2 < stop_idx; idx2++){
             int index2 = index[idx2];
             {0} data2 = data[idx2];
+            long long int outidx2 = \
+                static_cast<long long int>(index1) * ncols + index2;
+            atomicAdd(&out[outidx2], data1 * data2);
+        }
+    }
+}
+"""
+
+cov_kernel_str_sparse = r"""
+(int *indptr_x, int *index_x, {0} *data_x, \
+    int *indptr_y, int *index_y, {0} *data_y, \
+    int nrows, int ncols, {0} *out) {
+    int row = blockDim.x * blockIdx.x + threadIdx.x;
+    if(row >= nrows) return;
+    int start_idx_x = indptr_x[row];
+    int stop_idx_x = indptr_x[row+1];
+
+    int start_idx_y = indptr_y[row];
+    int stop_idx_y = indptr_y[row+1];
+
+    for(int idx = start_idx_x; idx < stop_idx_x; idx++){
+        int index1 = index_x[idx];
+        {0} data1 = data_x[idx];
+        for(int idx2 = start_idx_y; idx2 < stop_idx_y; idx2++){
+            int index2 = index_y[idx2];
+            {0} data2 = data_y[idx2];
             long long int outidx2 = \
                 static_cast<long long int>(index1) * ncols + index2;
             atomicAdd(&out[outidx2], data1 * data2);
@@ -66,9 +91,15 @@ def _cov_kernel(dtype):
     return cuda_kernel_factory(cov_kernel_str, (dtype,), "cov_kernel")
 
 
-def _mean_cov_kernel(dtype):
+def _cov_kernel_sparse_xx(dtype):
     return cuda_kernel_factory(
-        mean_cov_kernel_str, (dtype,), "mean_cov_kernel"
+        cov_kernel_str_sparse_xx, (dtype,), "cov_kernel_sprase_xx"
+    )
+
+
+def _cov_kernel_sprase(dtype):
+    return cuda_kernel_factory(
+        cov_kernel_str_sparse, (dtype,), "cov_kernel_sprase"
     )
 
 
@@ -134,13 +165,15 @@ def cov(x, y, mean_x=None, mean_y=None, return_gram=False, return_mean=False):
         )
 
     # Fix for cupy issue #7699: addressing problems with sparse matrix multiplication (spGEMM)
-    if (
-        x is y
-        and cupyx.scipy.sparse.issparse(x)
-        and mean_x is None
-        and mean_y is None
-    ):
-        return _cov_sparse(x, return_gram=return_gram, return_mean=return_mean)
+    if cupyx.scipy.sparse.issparse(x) and cupyx.scipy.sparse.issparse(y):
+        return _cov_sparse(
+            x,
+            y,
+            mean_x,
+            mean_y,
+            return_gram=return_gram,
+            return_mean=return_mean,
+        )
 
     if mean_x is not None and mean_y is not None:
         if mean_x.dtype != mean_y.dtype:
@@ -199,10 +232,22 @@ def cov(x, y, mean_x=None, mean_y=None, return_gram=False, return_mean=False):
 
 
 @cuml.internals.api_return_any()
-def _cov_sparse(x, return_gram=False, return_mean=False):
+def _cov_sparse(
+    x, y, mean_x=None, mean_y=None, return_gram=False, return_mean=False
+):
     """
-    Computes the mean and the covariance of matrix X of
-    the form Cov(X, X) = E(XX) - E(X)E(X)
+    Computes a covariance between two matrices using
+    the form Cov(X, Y) = E(XY) - E(X)E(Y)
+
+    This function prevents the need to explicitly
+    compute the outer product E(X)E(Y) by taking
+    advantage of the symmetry of that matrix
+    and computes per element in a kernel.
+
+    When E(XY) is approximately equal to E(X)E(Y),
+    this method is prone to catastrophic cancellation.
+    In such cases, a rectangular solver should be
+    preferred.
 
     This is a temporary fix for cupy issue #7699, where the
     operation `x.T.dot(x)` did not work for larger
@@ -211,9 +256,16 @@ def _cov_sparse(x, return_gram=False, return_mean=False):
     Parameters
     ----------
 
-    x : cupyx.scipy.sparse of size (m, n)
+    x : device-array or cupyx.scipy.sparse of size (m, n)
+    y : device-array or cupyx.scipy.sparse of size (m, n)
+    mean_x : float (default = None)
+        device-array of size (n, ) which is the mean
+        of x across rows
+    mean_x : float (default = None)
+        device-array of size (n, ) which is the mean
+        of x across rows
     return_gram : boolean (default = False)
-        If True, gram matrix of the form (1 / n) * X.T.dot(X)
+        If True, gram matrix of the form (1 / n) * X.T.dot(Y)
         will be returned.
         When True, a copy will be created
         to store the results of the covariance.
@@ -221,49 +273,74 @@ def _cov_sparse(x, return_gram=False, return_mean=False):
         will be overwritten
     return_mean: boolean (default = False)
         If True, the Maximum Likelihood Estimate used to
-        calculate the mean of X and X will be returned,
-        of the form (1 / n) * mean(X) and (1 / n) * mean(X)
+        calculate the mean of X and Y will be returned,
+        of the form (1 / n) * mean(X) and (1 / n) * mean(Y)
 
     Returns
     -------
 
-    result : cov(X, X) when return_gram and return_mean are False
-            cov(X, X), gram(X, X) when return_gram is True,
-            return_mean is False
-            cov(X, X), mean(X), mean(X) when return_gram is False,
-            return_mean is True
-            cov(X, X), gram(X, X), mean(X), mean(X)
-            when return_gram is True and return_mean is True
+    result : cov(X, Y) when return_gram and return_mean are False
+             cov(X, Y), gram(X, Y) when return_gram is True,
+                return_mean is False
+             cov(X, Y), mean(X), mean(Y) when return_gram is False,
+                return_mean is True
+             cov(X, Y), gram(X, Y), mean(X), mean(Y)
+                when return_gram is True and return_mean is True
+
     """
     if not cupyx.scipy.sparse.isspmatrix_csr(x):
         x = x.tocsr()
-    gram_matrix = cp.zeros((x.shape[1], x.shape[1]), dtype=x.data.dtype)
-    mean_x = cp.zeros((x.shape[1],), dtype=x.data.dtype)
+    if not cupyx.scipy.sparse.isspmatrix_csr(y):
+        y = y.tocsr()
+    gram_matrix = cp.zeros((x.shape[1], y.shape[1]), dtype=x.data.dtype)
 
     block = (8,)
     grid = (math.ceil(x.shape[0] / block[0]),)
-    compute_mean_cov = _mean_cov_kernel(x.data.dtype)
-    compute_mean_cov(
-        grid,
-        block,
-        (
-            x.indptr,
-            x.indices,
-            x.data,
-            x.shape[0],
-            x.shape[1],
-            gram_matrix,
-            mean_x,
-        ),
-    )
-    gram_matrix = gram_matrix + gram_matrix.T
-    gram_matrix -= cp.diag(cp.diag(gram_matrix) / 2)
+    if x is y:
+        compute_mean_cov = _cov_kernel_sparse_xx(x.data.dtype)
+        compute_mean_cov(
+            grid,
+            block,
+            (
+                x.indptr,
+                x.indices,
+                x.data,
+                x.shape[0],
+                x.shape[1],
+                gram_matrix,
+            ),
+        )
+        gram_matrix = gram_matrix + gram_matrix.T
+
+    else:
+        compute_mean_cov = _cov_kernel_sprase(x.data.dtype)
+        compute_mean_cov(
+            grid,
+            block,
+            (
+                x.indptr,
+                x.indices,
+                x.data,
+                y.indptr,
+                y.indices,
+                y.data,
+                x.shape[0],
+                y.shape[1],
+                gram_matrix,
+            ),
+        )
+
     gram_matrix *= 1 / x.shape[0]
-    mean_x *= 1 / x.shape[0]
+
+    if mean_x is None:
+        mean_x = x.sum(axis=0) * (1 / x.shape[0])
+
+    if mean_y is None:
+        mean_y = y.sum(axis=0) * (1 / y.shape[0])
 
     if return_gram:
         cov_result = cp.zeros(
-            (gram_matrix.shape[0], gram_matrix.shape[0]),
+            (gram_matrix.shape[0], gram_matrix.shape[1]),
             dtype=gram_matrix.dtype,
         )
     else:
@@ -272,11 +349,14 @@ def _cov_sparse(x, return_gram=False, return_mean=False):
     compute_cov = _cov_kernel(x.dtype)
 
     block_size = (8, 8)
-    grid_size = (math.ceil(gram_matrix.shape[0] / 8),) * 2
+    grid_size = (
+        math.ceil(gram_matrix.shape[0] / 8),
+        math.ceil(gram_matrix.shape[1] / 8),
+    )
     compute_cov(
         grid_size,
         block_size,
-        (cov_result, gram_matrix, mean_x, mean_x, gram_matrix.shape[0]),
+        (cov_result, gram_matrix, mean_x, mean_y, gram_matrix.shape[0]),
     )
 
     if not return_gram and not return_mean:
@@ -284,6 +364,6 @@ def _cov_sparse(x, return_gram=False, return_mean=False):
     elif return_gram and not return_mean:
         return cov_result, gram_matrix
     elif not return_gram and return_mean:
-        return cov_result, mean_x, mean_x
+        return cov_result, mean_x, mean_y
     elif return_gram and return_mean:
-        return cov_result, gram_matrix, mean_x, mean_x
+        return cov_result, gram_matrix, mean_x, mean_y
